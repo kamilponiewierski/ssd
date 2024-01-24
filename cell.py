6c881a5ac702e176b0734cfbc2a8992af36ecaf7
Shikoqu <92339252+Shikoqu@users.noreply.github.com>
2024-01-24 21:38:00 +0100

    Model 3ab and resizing pygame (#4)
    Co-authored-by: Kamil Poniewierski <kamiljanponiewierski@gmail.com>

--- cell.py
+++ cell.py
@@ -9,10 +9,12 @@ class Cell(ABC):
     growth_stage: int
     position: (int, int)
 
-    def grow_shroom(self, neighbourhood: np.array, global_growth_chance: float) -> int:
+    def grow_shroom(
+        self, neighbourhood: np.array, global_growth_chance: float, direction: int
+    ) -> int:
         new_growth_stage = self.growth_stage
 
-        model_growth_chance = self.calculate_growth_chance(neighbourhood)
+        model_growth_chance = self.calculate_growth_chance(neighbourhood, direction)
         if self.growth_probability > global_growth_chance:
             if model_growth_chance > np.random.random():
                 new_growth_stage += 1
@@ -20,7 +22,7 @@ class Cell(ABC):
         return new_growth_stage
 
     @abstractmethod
-    def calculate_growth_chance(self, neighbourhood: np.array) -> float:
+    def calculate_growth_chance(self, neighbourhood: np.array, direction: int) -> float:
         pass
 
     def __init__(self, position: (int, int)) -> None:
@@ -33,8 +35,6 @@ class Cell(ABC):
 class CellModel1a(Cell):
     def _calculate_growth_chance(self, neighbour_count):
         match neighbour_count:
-            case 0:
-                return 0.0
             case 1:
                 return 0.125
             case 2:
@@ -44,28 +44,35 @@ class CellModel1a(Cell):
             case _:
                 return 0.0
 
-    def calculate_growth_chance(self, neighbourhood: np.array) -> float:
+    def calculate_growth_chance(self, neighbourhood: np.array, direction: int) -> float:
         if self.growth_stage > 0:
             return 0.0
 
-        shroom_count = 0
-
-        for cell in neighbourhood.flat:
-            if cell.growth_stage > 0:
-                shroom_count += 1
+        shroom_count = sum(1 for cell in neighbourhood.flat if cell.growth_stage > 0)
 
         return self._calculate_growth_chance(shroom_count)
+
+
+class CellModel1b(CellModel1a):
+    def _calculate_growth_chance(self, neighbour_count):
+        match neighbour_count:
+            case 1:
+                return 0.500
+            case 2:
+                return 0.250
+            case 3:
+                return 0.125
+            case _:
+                return 0.000
 
 
 class CellModel1c(CellModel1a):
     def _calculate_growth_chance(self, neighbour_count):
         match neighbour_count:
-            case 0:
-                return 0.0
             case 1:
                 return 0.125
             case _:
-                return 0.0
+                return 0.000
 
 
 class CellModel2a(CellModel1c):
@@ -73,10 +80,12 @@ class CellModel2a(CellModel1c):
         super().__init__(position)
         self.maturity_phase = 0
 
-    def grow_shroom(self, neighbourhood: np.array, global_growth_chance: float) -> int:
+    def grow_shroom(
+        self, neighbourhood: np.array, global_growth_chance: float, direction: int = -1
+    ) -> int:
         new_growth_stage = self.growth_stage
 
-        model_growth_chance = self.calculate_growth_chance(neighbourhood)
+        model_growth_chance = self.calculate_growth_chance(neighbourhood, direction)
         if self.growth_probability > global_growth_chance:
             if model_growth_chance > np.random.random():
                 if self.maturity_phase < 4:
@@ -84,7 +93,9 @@ class CellModel2a(CellModel1c):
 
         return new_growth_stage
 
-    def matureUnocupiedCell(self, window: np.array(Cell), global_mature_chance: float) -> None:
+    def matureUnocupiedCell(
+        self, window: np.array(Cell), global_mature_chance: float
+    ) -> None:
         ocupiedCellsNumber = 0
 
         for cell in window.flatten():
@@ -96,9 +107,7 @@ class CellModel2a(CellModel1c):
             if random.random() < matureChance:
                 self.maturity_phase += 1
 
-
     def calculateMatureChance(self, ocupiedCellsNumber: int):
-
         match ocupiedCellsNumber:
             case 0:
                 return 0.0
@@ -110,5 +119,55 @@ class CellModel2a(CellModel1c):
                 return 1.0
 
 
+class CellModel3a(Cell):
+    def calculate_growth_chance(self, neighbourhood: np.array, direction: int) -> float:
+        if self.growth_stage > 0:
+            return 0.0
+
+        shroom_count = sum(1 for cell in neighbourhood.flat if cell.growth_stage > 0)
+
+        def neighbour_in_direction_stage() -> int:
+            match direction:
+                case 0:
+                    return neighbourhood[0, 1].growth_stage
+                case 1:
+                    return neighbourhood[0, 2].growth_stage
+                case 2:
+                    return neighbourhood[1, 2].growth_stage
+                case 3:
+                    return neighbourhood[2, 2].growth_stage
+                case 4:
+                    return neighbourhood[2, 1].growth_stage
+                case 5:
+                    return neighbourhood[2, 0].growth_stage
+                case 6:
+                    return neighbourhood[1, 0].growth_stage
+                case 7:
+                    return neighbourhood[0, 0].growth_stage
+                case _:
+                    raise ValueError(f"Invalid direction {direction}")
+
+        if neighbour_in_direction_stage() < 1:
+            return 0.0
+
+        return self._calculate_growth_chance(shroom_count)
+
+    def _calculate_growth_chance(self, neighbour_count):
+        match neighbour_count:
+            case 1:
+                return 0.5
+            case _:
+                return 0.0
 
 
+class CellModel3b(CellModel3a):
+    def _calculate_growth_chance(self, neighbour_count):
+        match neighbour_count:
+            case 1:
+                return 0.500
+            case 2:
+                return 0.250
+            case 3:
+                return 0.125
+            case _:
+                return 0.000
--- program.py
+++ program.py
@@ -3,14 +3,15 @@ import pickle
 import time
 import numpy as np
 
-from cell import CellModel1a, CellModel1c, CellModel2a
+from cell import CellModel1a, CellModel1c, CellModel2a, CellModel3a, CellModel3b
+
 
 SIZE_Y = 320
 SIZE_X = 200
+CellType = CellModel3a
 
 # TODO maybe add CLI support?
-OUTPUT_PATH = 'growth_log_2a.pkl'
-
+OUTPUT_PATH = 'growth_log_3a.pkl'
 
 
 def create_cell_array() -> np.array:
@@ -18,7 +19,7 @@ def create_cell_array() -> np.array:
     arr = np.empty(shape=(x, y), dtype=object)
     for i in range(x):
         for j in range(y):
-            arr[i][j] = CellModel2a((i, j))
+            arr[i][j] = CellType((i, j))
 
     center_x, center_y = x // 2, y // 2
     arr[center_x, center_y].growth_stage = 1
@@ -30,19 +31,20 @@ def save_array_state(arr, file):
     pickle.dump(vectorized_stage(arr), file)
 
 
-def step(arr):
+def step(arr, direction):
     new_arr = np.zeros(arr.shape, dtype=np.int16)
     y_size, x_size = arr.shape
     global_grow_chance = np.random.normal()
 
-    for row in range(1, y_size-1):
-        for col in range(1, x_size-1):
-            window = arr[row-1:row+2, col-1:col+2]
+    for row in range(1, y_size - 1):
+        for col in range(1, x_size - 1):
+            window = arr[row - 1 : row + 2, col - 1 : col + 2]
             new_arr[row][col] = arr[row][col].grow_shroom(
-                window, global_grow_chance)
+                window, global_grow_chance, direction
+            )
 
-    for row in range(1, y_size-1):
-        for col in range(1, x_size-1):
+    for row in range(1, y_size - 1):
+        for col in range(1, x_size - 1):
             arr[row][col].growth_stage = new_arr[row][col]
 
 
@@ -56,11 +58,10 @@ def reroll_probabilities(arr):
 
 
 
-def mature_unocupied_cells(arr: np.array(CellModel2a) ) -> None:
+def mature_unocupied_cells(arr: np.array(CellModel2a)) -> None:
     # with each step it is obligated to mature unoccupied cells in 2a model
     y_size, x_size = arr.shape
     global_mature_chance = np.random.normal()
-
 
     for row in range(1, y_size - 1):
         for col in range(1, x_size - 1):
@@ -72,7 +73,7 @@ def mature_unocupied_cells(arr: np.array(CellModel2a) ) -> None:
 
 if __name__ == "__main__":
     cells = create_cell_array()
-    with lzma.open(OUTPUT_PATH, 'wb') as output:
+    with lzma.open(OUTPUT_PATH, "wb") as output:
         generation_i = 0
         while True:
             generation_i += 1
@@ -83,18 +84,18 @@ if __name__ == "__main__":
                 mature_unocupied_cells(cells)
 
             reroll_probabilities(cells)
-            step(cells)
+            step(cells, generation_i % 8)
             save_array_state(cells, output)
 
             reached_border = False
 
             for border in [cells[1, :], cells[-2, :], cells[:, 1], cells[:, -2]]:
                 for cell in border:
-                    if cell.growth_stage == 1 :
+                    if cell.growth_stage > 0:
                         reached_border = True
                         break
 
             if reached_border:
                 break
 
-            print(f'Generation {generation_i}, took {time.time() - start}s')
+            print(f"Generation {generation_i}, took {time.time() - start}s")
