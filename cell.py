5ab878e13533dc65987ba673cbe22984d2a15125
Krystian <krystiano1602@gmail.com>
2024-01-24 06:16:20 +0100

    Nie działa :))))))

--- cell.py
+++ cell.py
@@ -68,53 +68,48 @@ class CellModel1c(CellModel1a):
                 return 0.0
 
 
-class CellModel2a(Cell):
+class CellModel2a(CellModel1c):
     def __init__(self, position: (int, int)) -> None:
         super().__init__(position)
         self.maturity_phase = 0
 
     def grow_shroom(self, neighbourhood: np.array, global_growth_chance: float) -> int:
+        new_growth_stage = self.growth_stage
+
         model_growth_chance = self.calculate_growth_chance(neighbourhood)
-
         if self.growth_probability > global_growth_chance:
-            if model_growth_chance > random.random():
-                if self.maturity_phase != 3:
-                    self.growth_stage = 1
-                    return 1
+            if model_growth_chance > np.random.random():
+                if self.maturity_phase < 3:
+                    self.growth_stage += 1
+                    new_growth_stage += 1
 
-        return 0
+        return new_growth_stage
 
-    def olderUocupiedCells(self, window: np.array(Cell)) -> None:
-        mature_chance = self.calculate_mature_chance(window)
-        if random.random() < mature_chance and self.maturity_phase != 3 and self.growth_stage != 1:
-            self.maturity_phase += 1
+    def matureUnocupiedCell(self, window: np.array(Cell), global_mature_chance: float) -> None:
+        ocupiedCellsNumber = 0
 
-    def calculate_growth_chance(self, window: np.array(Cell)) -> float:
-        number_of_occupated_cells_in_window = 0
+        for cell in window.flatten():
+            if cell.growth_stage > 0:
+                ocupiedCellsNumber += 1
 
-        for cell in np.nditer(window):
-            if cell.growth_stage == 1:
-                number_of_occupated_cells_in_window += 1
+        matureChance = self.calculateMatureChance(ocupiedCellsNumber)
+        if self.growth_probability > global_mature_chance:
+            if random.random() < matureChance:
+                self.maturity_phase += 1
 
-        match number_of_occupated_cells_in_window:
-            case 1:
-                return 0.125
-            case _:
+
+    def calculateMatureChance(self, ocupiedCellsNumber: int):
+
+        match ocupiedCellsNumber:
+            case 0:
                 return 0.0
-
-    def calculate_mature_chance(self, window: np.array(Cell)) -> float:
-        number_of_occupated_cells_in_window = 0
-
-        for cell in np.nditer(window):
-            if cell.growth_stage == 1:
-                number_of_occupated_cells_in_window += 1
-
-        match number_of_occupated_cells_in_window:
             case 1:
                 return 0.5
             case 2:
                 return 0.75
             case _:
                 return 1.0
+
+
 
 
